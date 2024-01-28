17856fa0e593ef8ad5ec5f409042b2861acdf268
zubair <zubairahmad29278@gmail.com>
2024-01-28 13:11:59 -0700

    requirement done

--- src/app/vehical-listing/required/required.component.ts
+++ src/app/vehical-listing/required/required.component.ts
@@ -11,5 +11,20 @@ export class RequiredComponent implements OnInit {
 
   ngOnInit(): void {
   }
+car =[
+   
+     { name:"swift",liter:'90L',manual:"manual",price:"4000/",people:"4" },
+     { name:"honda",liter:'80L',manual:"manual",price:"8000/",people:"3" },
+     { name:"Mehran",liter:'70L',manual:"manual",price:"5000/",people:"5" },
+     { name:"Frarri",liter:'70L',manual:"manual",price:"3000/" ,people:"4"},
+     { name:"swift",liter:'90L',manual:"manual",price:"4000/",people:"4" },
+     { name:"honda",liter:'80L',manual:"manual",price:"8000/",people:"3" },
+     { name:"Mehran",liter:'70L',manual:"manual",price:"5000/",people:"5" },
+     { name:"Frarri",liter:'70L',manual:"manual",price:"3000/" ,people:"4"},
+     { name:"swift",liter:'90L',manual:"manual",price:"4000/",people:"4" },
+     { name:"honda",liter:'80L',manual:"manual",price:"8000/",people:"3" },
+     { name:"Mehran",liter:'70L',manual:"manual",price:"5000/",people:"5" },
+     { name:"Frarri",liter:'70L',manual:"manual",price:"3000/" ,people:"4"},
+  ]
 
 }
